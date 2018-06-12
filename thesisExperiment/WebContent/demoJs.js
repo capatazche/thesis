214e13d85b8bd618ad840662288a7efbe8a0dcba
'capatazche' <'bvillalba13@hotmail.com'>
2018-06-12 19:21:41 +0100

    Country of employment, extra field in salaray options in Demographics

--- thesisExperiment/WebContent/demoJs.js
+++ thesisExperiment/WebContent/demoJs.js
@@ -16,3 +16,23 @@ function englishFirstLang(){
 	
 	return;
 }
+
+function changeInStatus(){
+	var salaryRangeDiv = document.getElementById('salaryRange');
+	var countryEmployedInDiv = document.getElementById('countryEmployedIn');
+	
+	//Suggested range taken from https://www.surveygizmo.com/resources/blog/how-to-write-better-demographic-questions/
+	if (document.getElementById('workingStatus').checked){
+		salaryRangeDiv.innerHTML = '<fieldset><label>Yearly Salary Range In USD</label><br><select><option value="less10k">Less than $10,000</option><option value="10kto19k">$10,000 to $19,999</option><option value="20kTo34k">$20,000 to $34,999</option><option value="35kTo49k">$35,000 to $49,999</option><option value="50kTo74k">$50,000 to $74,999</option><option value="75kTo99k">$75,000 to $99,999</option><option value="over100k">Over $100,000</option></select></fieldset><br>';
+		
+		countryEmployedInDiv.innerHTML = '<fieldset><label>Country you are employed in</label><br><select><option value="mexico">Mexico</option><option value="unitedStates">United States</option></select></fieldset><br>';
+	}
+	
+	else{
+		salaryRangeDiv.innerHTML = '';
+		
+		countryEmployedInDiv.innerHTML = '';
+	}
+	
+	return;
+}
